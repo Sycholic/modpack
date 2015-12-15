938f09ada8ce3b0ace8f48bc74c5a868a348027d
ShiFoo <JReichwald@gmail.com>
2015-12-16 00:04:20 +0100

    Fixed unit names, init lines, remote exec modes and spawn locations
    #121

---
+++ [BWI]Template.vr/BWI/gui/BWIDefines.hpp
@@ -0,0 +1,95 @@
+class BWITreeView
+{
+	access = 0; // Control access (0 - ReadAndWrite, 1 - ReadAndCreate, 2 - ReadOnly, 3 - ReadOnlyVerified)
+	idc = -1;
+	type = CT_TREE; // Type
+	style = ST_LEFT; // Style
+	default = 0; // Control selected by default (only one within a display can be used)
+	blinkingPeriod = 0; // Time in which control will fade out and back in. Use 0 to disable the effect.
+	
+	x = 0; // Horizontal coordinates
+	y = 0; // Vertical coordinates
+	w = 0; // Width
+	h = 0; // Height
+
+	colorBorder[] = {0.5,0.5,0.5,1}; // Frame color
+	colorBackground[] = {0.1,0.1,0.1,1}; // Fill color
+	colorSelect[] = {0.275,0.463,0.694,1}; // Selected item fill color (when multiselectEnabled is 0)
+	colorMarked[] = {0.275,0.463,0.694,1}; // Selected item fill color (when multiselectEnabled is 0)
+	colorText[] = {1,1,1,1}; // Text color
+	colorSelectText[] = {1,1,1,1}; // Selected text color (when multiselectEnabled is 0)
+	colorMarkedText[] = {1,1,1,1}; // Selected text color (when multiselectEnabled is 1)
+	colorMarkedSelected[] = {0.275,0.463,0.694,1}; // Selected item fill color (when multiselectEnabled is 1)
+
+	sizeEx = GUI_GRID_CENTER_H; // Text size
+	font = GUI_FONT_NORMAL; // Font from CfgFontFamilies
+	shadow = 1; // Shadow (0 - none, 1 - N/A, 2 - black outline)
+
+	multiselectEnabled = 0; // Allow selecting multiple items while holding Ctrl or Shift
+	expandOnDoubleclick = 0; // Expand/collapse item upon double-click
+	hiddenTexture = "A3\ui_f\data\gui\rsccommon\rsctree\hiddenTexture_ca.paa"; // Expand icon
+	expandedTexture = "A3\ui_f\data\gui\rsccommon\rsctree\expandedTexture_ca.paa"; // Collapse icon
+	maxHistoryDelay = 1; // Time since last keyboard type search to reset it
+
+	// Scrollbar configuration
+	class ScrollBar
+	{
+		width = 0; // Unknown?
+		height = 0; // Unknown?
+		scrollSpeed = 0.01; // Unknown?
+
+		arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa"; // Arrow
+		arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa"; // Arrow when clicked on
+		border = "\A3\ui_f\data\gui\cfg\scrollbar\border_ca.paa"; // Slider background (stretched vertically)
+		thumb = "\A3\ui_f\data\gui\cfg\scrollbar\thumb_ca.paa"; // Dragging element (stretched vertically)
+
+		color[] = {1,1,1,1}; // Scrollbar color
+	};
+
+	colorDisabled[] = {0,0,0,0}; // Does nothing, but must be present, otherwise an error is shown
+	colorArrow[] = {0,0,0,0}; // Does nothing, but must be present, otherwise an error is shown
+};
+
+class BWIXListbox
+{
+	access = 0; // Control access (0 - ReadAndWrite, 1 - ReadAndCreate, 2 - ReadOnly, 3 - ReadOnlyVerified)
+	idc = CT_XLISTBOX; // Control identification (without it, the control won't be displayed)
+	type = CT_XLISTBOX; // Type
+	style = SL_HORZ + ST_CENTER + LB_TEXTURES; // Style
+	default = 0; // Control selected by default (only one within a display can be used)
+	blinkingPeriod = 0; // Time in which control will fade out and back in. Use 0 to disable the effect.
+
+	x = 0; // Horizontal coordinates
+	y = 0; // Vertical coordinates
+	w = 0; // Width
+	h = 0; // Height
+
+	color[] = {1,1,1,1}; // Arrow color
+	colorActive[] = {1,1,1,1}; // Selected arrow color
+
+	sizeEx = GUI_GRID_CENTER_H; // Text size
+	font = GUI_FONT_NORMAL; // Font from CfgFontFamilies
+	shadow = 0; // Shadow (0 - none, 1 - N/A, 2 - black outline)
+	colorText[] = {1,1,1,1}; // Text color
+	colorSelect[] = {1,1,1,1}; // Selected text color
+	colorDisabled[] = {1,1,1,0.5}; // Disabled text color
+
+	arrowEmpty = "\A3\ui_f\data\gui\cfg\slider\arrowEmpty_ca.paa"; // Arrow
+	arrowFull = "\A3\ui_f\data\gui\cfg\slider\arrowFull_ca.paa"; // Arrow when clicked on
+	border = "\A3\ui_f\data\gui\cfg\slider\border_ca.paa"; // Fill texture
+
+	soundSelect[] = {"\A3\ui_f\data\sound\RscListbox\soundSelect",0.09,1}; // Sound played when an item is selected
+};
+
+class BWIButton: RscButton
+{
+	blinkingPeriod = 0; // Time in which control will fade out and back in. Use 0 to disable the effect.
+	colorFocused[] = {0.275,0.463,0.694,1};
+	colorText[] = {1,1,1,1};
+	colorBackground[] = {0.275,0.463,0.694,1};
+	colorActive[] = {0.275,0.463,0.694,1};
+	colorBackgroundActive[] = {0.6,0.78,1,1};
+	period = 0; // Oscillation time between colorBackground and colorFocused when selected
+	sizeEx = GUI_GRID_CENTER_H; // Text size
+	font = GUI_FONT_NORMAL; // Font from CfgFontFamilies	
+};
--- [BWI]Template.vr/onPlayerRespawn.sqf
+++ [BWI]Template.vr/onPlayerRespawn.sqf
@@ -24,3 +24,5 @@ if( player in [z1,z2,z3,z4,z5,z6,z7,z8,z9,z10,z11,z12,z13,z14,z15,z16] ) then {
 	AdminAction2 = SpawnVAS addAction[ "<t color='#11c311'>BWI Admin: End Mission (Success)</t>", "[true] remoteExecCall ['BWI_fnc_EndMission']" ];
 	AdminAction3 = SpawnVAS addAction[ "<t color='#bb1111'>BWI Admin: End Mission (Failure)</t>", "[false] remoteExecCall ['BWI_fnc_EndMission']" ];
 };
+
+player addAction ["BWI: CommCard", "createDialog 'ShowCommCard';", nil, -10, false, false];
